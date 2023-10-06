3ce7ec3a92dabb457a9fa00573aea829dab50c8e
Patrick Auld <patrick@patrickauld.com>
2023-10-06 11:07:01 -0700

    inital content

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,69 @@
-import Image from 'next/image'
+// pages/index.tsx
 
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative">
+      {/* Header overlayed on splash image */}
+
+       <div className="relative h-screen flex items-center justify-center bg-cover bg-center"
+                 style={{ backgroundImage: 'url(/splash.png)' }}>
+
+                    <header className="p-4 text-white">
+                          <div className="container mx-auto">
+                            Daylight Smearings
+                          </div>
+                        </header>
+      </div>
+
+
+      {/* Section 1: Slow Time Change */}
+      <section className="flex flex-col md:flex-row items-center my-16">
+        <img src="/time-change.png" alt="Time Change" className="md:w-1/2 w-full object-cover" />
+        <div className="md:w-1/2 p-8">
+          <h2 className="text-2xl font-bold mb-4">Ease into Change</h2>
+          <p>
+            Today's technology can softly guide you through a time change.
+            Jarring clocks by 60 minutes in the middle of the night hurts people and communities.
+            Clocks today can speed up or slow down by just 12 seconds an hour, for a month.
+            You can wake up at nearly the same time everyday, gradually guiding you biological clock to the new time.
+          </p>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      {/* Section 2: Health Benefits */}
+      <section className="flex flex-col md:flex-row-reverse items-center my-16">
+        <img src="/health-benefits.png" alt="Health Benefits" className="md:w-1/2 w-full object-cover" />
+        <div className="md:w-1/2 p-8">
+          <h2 className="text-2xl font-bold mb-4">Walk, don't Jump</h2>
+          <p>
+            Gradually bring yourself into Daylight Savings with Daylight Smearings.
+            No more groggy mornings and mixed up alarms.
+            Don't wonder what time you have to get to work.
           </p>
-        </a>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+      {/* Section 3: Call to Action */}
+      <section className="flex flex-col md:flex-row items-center my-16">
+        <img src="/call-to-action.png" alt="Call to Action" className="md:w-1/2 w-full object-cover" />
+        <div className="md:w-1/2 p-8">
+          <h2 className="text-2xl font-bold mb-4">Take Action</h2>
+          <p>
+            Sign up to be notified of smearing progress and to get our mobile app with smearing alarms.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          {/* Mailing List Signup Form (Placeholder) */}
+          <form className="mt-4">
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="p-2 border border-gray-300 rounded mb-2 w-full"
+            />
+            <button type="submit" className="w-full py-2 bg-blue-600 text-white rounded">Sign Up</button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 }
+
+export default Home;
